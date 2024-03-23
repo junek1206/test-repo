cc64f086df1c460c53849ac71f2d7235cafa77a1
junek1206hufs <163493943+junek1206hufs@users.noreply.github.com>
2024-03-23 17:35:29 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,6 +2,6 @@
 
 int main() {
   std::cout << "Hello, world!" << std::endl;
-  cout << "save the world, my final message. goodbye." << endl;
+  std::cout << "save the world, my final message. goodbye." << std::endl;
   return 0;
 }
